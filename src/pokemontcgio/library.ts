c4860509e51913bfa2e8a4bbcf1985e712e3c50e
SquarePear <16364318+SquarePear@users.noreply.github.com>
2020-08-26 18:20:00 +0000

    Added getCard and getCards

---
+++ src/pokemontcgio/library.ts
@@ -0,0 +1,30 @@
+import { Card } from "./typings.ts";
+
+export async function getCard(id: string): Promise<Card> {
+  const response = await fetch(
+    `https://api.pokemontcg.io/v1/cards/${id}`,
+  );
+
+  const card: Card = (await response.json()).card;
+
+  return card;
+}
+
+export async function getCards(cardParam: Card): Promise<Card[]> {
+  let search = "";
+
+  let keys = Object.keys(cardParam);
+  let values = Object.values(cardParam);
+
+  for (let i = 0; i < keys.length; i++) {
+    search += `${keys[i]}=${values.toString()}&`;
+  }
+
+  const response = await fetch(
+    `https://api.pokemontcg.io/v1/cards?${search}`,
+  );
+
+  const cards: Card[] = (await response.json()).cards;
+
+  return cards;
+}
